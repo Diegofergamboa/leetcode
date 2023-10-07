ab0e31ef32d84639a62c58abb6f613416f7b7a29
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2023-10-07 18:36:18 -0500

    Time: 57 ms (74.93%) | Memory: 44.2 MB (13.52%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,12 +3,25 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+var twoSum = function (nums, target) {
+    const numsWithIndices = nums.map((num, index) => ({ num, index }))
+    numsWithIndices.sort((a, b) => a.num - b.num)
+
+    let left = 0
+    let right = numsWithIndices.length - 1;
+
+    while (left < right) {
+        const sum = numsWithIndices[left].num + numsWithIndices[right].num;
+
+        if (sum === target) {
+            return [numsWithIndices[left].index, numsWithIndices[right].index];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
     }
-};
+
+    throw new Error('Solution not found');
+
+}

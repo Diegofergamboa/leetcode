cb147360e2fbb195864541779d83ec900e59936a
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2025-04-16 14:08:44 -0500

    Time: 3 ms (40.82%) | Memory: 12.6 MB (19.70%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        current = head
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next  # Saltear duplicado
+            else:
+                current = current.next
+        return head

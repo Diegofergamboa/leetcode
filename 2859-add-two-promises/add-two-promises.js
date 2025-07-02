054aef6954da37b5b8ed8202f8359d582fdcbc2e
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2025-07-02 11:22:09 -0500

    Time: 52 ms (54.82%) | Memory: 53.8 MB (67.63%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,8 +4,7 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const num1 = await promise1
-    const num2 = await promise2
+    const [num1, num2] = await Promise.all([promise1, promise2])
     return num1 + num2
 };
 
